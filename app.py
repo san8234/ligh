9f92f3511d58dd256a8f0ab6df95eb6dc82ff560
san8234 <sandeep.vijay.38412@gmail.com>
2025-05-14 21:54:17 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.title("Sandeep Funds")
+st.write("Kindly put pichai....any amount is appreciated")
+st.write("Gotta buy an Acer Nitro V16 powered by AMD ryzen 7 8845HS and RTX 4060")
+
+
+col1, col2 = st.columns(2)
+
+
+with col1:
+    if st.button("Kaasu Tharuven Sandeep!"):
+        st.success("kindly gpay 6383 434 045 your anbalipu")
+
+
+with col2:
+    if st.button("Poda dei no dhuddu for you!"):
+        st.success("Honestly I cant give 3 shits about your opinion kindly GPAY 6383 434 045 :)")
